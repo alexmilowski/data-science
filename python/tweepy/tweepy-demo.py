189a70192f79b6cafee866eb9c3383f76bc879d4
Gunnar Kleemann <gunnarkl@gmail.com>
2015-02-04 15:47:23 -0500

    assignment 1

--- python/tweepy/tweepy-demo.py
+++ python/tweepy/tweepy-demo.py
@@ -2,11 +2,11 @@ import tweepy
 
 # Note: You must fill in your four values below from your registered application.
 
-consumer_key = ""
-consumer_secret = ""
+consumer_key = "twI3B0GuFxkAY5Y8EggYuH1MW"
+consumer_secret = "MRnoOyoGOgoXAyd26Rwm2cZloVBT63MhZr5ZyvT06koxPDlIhV"
 
-access_token = ""
-access_token_secret = ""
+access_token = "720169710-4hstZM04S6DMKm1rr1y4Cy9TWvgTvu29LUKzbUT6"
+access_token_secret = "v23joaXipB0ORm5JCEEpR8xSOEwJWkNAXidpHRlElm3hY"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
