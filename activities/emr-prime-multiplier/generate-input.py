c304c79335efd4f351913bae7239ab537d816b55
XML Geek; runs with scissors <alex@milowski.com>
2015-03-17 15:24:23 -0700

    added documentation

--- activities/emr-prime-multiplier/generate-input.py
+++ activities/emr-prime-multiplier/generate-input.py
@@ -1,7 +1,9 @@
 import sys
 import random
 
+# The maximum magnitude of the numbers
 max = int(sys.argv[1])
+# The number to generate
 count = int(sys.argv[2])
 
 def positiveRandom(max):
@@ -11,5 +13,6 @@ def positiveRandom(max):
    r = int(n*max)
    return 1 if r==0 else r
    
+# Generate a positive random number for the count up to the given maximum
 for i in range(count):
    print positiveRandom(max)
--- activities/emr-prime-multiplier/prime-factors.py
+++ activities/emr-prime-multiplier/prime-factors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 
+# Appends a single next prime
 def appendPrime(primes):
    p = primes[-1]
    prime = False
@@ -16,24 +17,39 @@ def appendPrime(primes):
    primes.append(p)
    return primes
             
-         
+      
+# an initial set of primes   
 primes = [2, 3, 5]
+# an initial array of zeros of the same length
 counts = [0 for i in range(len(primes))]
+
+# for each line of input, factor the input
 for line in sys.stdin:
+
+   # Parse the integer and skip zeros
    i = int(line)
    if (i==0):
       continue
+      
+   # Factor until we reach 1
    p = 0;
    while i!=1:
       #print i,p,i % primes[p],counts
+      
+      # compute exponent for current prime
       while i!=1 and i % primes[p] == 0:
          i = i / primes[p]
          counts[p] += 1
+         
+      # increment prime
       p += 1
+      
+      # if we aren't at zero but have run out of primes, find the next prime to factor
       if i!=1 and p==len(primes):
          appendPrime(primes)
          counts.append(0)
-         
+
+# Output the counts for each prime         
 for i in range(len(primes)):
    if counts[i]>0:
       print "LongValueSum:",primes[i],"\t",counts[i]
